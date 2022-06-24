fe4ae7b2406f5953eac45f884ad7abf6a9a5721e
Maryadline <Cmaryadline@gmail.com>
2022-06-24 20:57:21 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
-let num1 = parseInt(prompt('Enter Number 1: ')) //use Number or parseInt
-let operator = prompt('Operator: (+, -, *, /) ') //use Number or parseInt
-let num2 = parseInt(prompt('Enter Number 2: '))
+let num1 = Number(prompt('Enter Number 1: '))
+let operator = prompt('Operator: (+, -, *, /) ')
+let num2 = Number(prompt('Enter Number 2: '))
 
 let result = (num1, num2) => {
     if (operator === '+') {
